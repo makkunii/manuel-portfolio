f84962ca6d7473331e4e7ca24e3b4f5d277bf631
makkunii <mace21218@gmail.com>
2023-03-05 07:52:55 +0400

    pwa not working

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,19 +1,19 @@
 const staticAnimeshData = "site-cache-v1";
 const assets = [
   //   "/",
-  "/index.html",
-  "/assets/css/styles.css",
-  "/assets/css/swiper-bundle.min.css",
-  "/assets/js/main.js",
-  "/assets/js/swiper-bundle.min.js",
-  "/assets/pdf/manuel_resume.pdf",
-  "/assets/img/blob.svg",
-  "/assets/img/hacker-profile.png",
-  "/assets/img/mt-fuji.jpg",
-  "/assets/img/skills/vue.svg",
-  "/assets/img/skills/javascript.svg",
-  "/assets/img/skills/html.svg",
-  "/assets/img/skills/css.svg",
+  "index.html",
+  "assets/css/styles.css",
+  "assets/css/swiper-bundle.min.css",
+  "assets/js/main.js",
+  "assets/js/swiper-bundle.min.js",
+  "assets/pdf/manuel_resume.pdf",
+  "assets/img/blob.svg",
+  "assets/img/hacker-profile.png",
+  "assets/img/mt-fuji.jpg",
+  "assets/img/skills/vue.svg",
+  "assets/img/skills/javascript.svg",
+  "assets/img/skills/html.svg",
+  "assets/img/skills/css.svg",
 ];
 
 self.addEventListener("install", (installEvent) => {
