0c3123564426b9cad08c8d9543b9b5465e3ad596
makkunii <mace21218@gmail.com>
2023-03-18 02:15:08 +0400

    colo changing

---
+++ serviceWorker.js
@@ -0,0 +1,33 @@
+// const staticAnimeshData = "site-cache-v1";
+// const assets = [
+//   //   "/",
+//   "index.html",
+//   "assets/css/styles.css",
+//   "assets/css/swiper-bundle.min.css",
+//   "assets/js/main.js",
+//   "assets/js/swiper-bundle.min.js",
+//   "assets/pdf/manuel_resume.pdf",
+//   "assets/img/blob.svg",
+//   "assets/img/hacker-profile.png",
+//   "assets/img/mt-fuji.jpg",
+//   "assets/img/skills/vue.svg",
+//   "assets/img/skills/javascript.svg",
+//   "assets/img/skills/html.svg",
+//   "assets/img/skills/css.svg",
+// ];
+
+// self.addEventListener("install", (installEvent) => {
+//   installEvent.waitUntil(
+//     caches.open(staticAnimeshData).then((cache) => {
+//       cache.addAll(assets);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", (fetchEvent) => {
+//   fetchEvent.respondWith(
+//     caches.match(fetchEvent.request).then((res) => {
+//       return res || fetch(fetchEvent.request);
+//     })
+//   );
+// });
